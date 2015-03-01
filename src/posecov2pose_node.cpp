02ba9ed800bf741a30fd500f6eab96aaeec0edeb
hhony <hans.hony@gmail.com>
2015-03-01 15:23:56 -0800

    posecov2pose: fixed pose output - should be input to rtab

--- src/posecov2pose_node.cpp
+++ src/posecov2pose_node.cpp
@@ -24,16 +24,20 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 #include "ros/ros.h"
-#include "geometry_msgs/PoseWithCovariance.h"
-#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
+#include "geometry_msgs/PoseStamped.h"
 #include <sstream>
 
 // the odometry_combined publish
 ros::Publisher odom_pub;
 
-void poseCallback(const geometry_msgs::PoseWithCovariance::ConstPtr& msg)
+void poseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg)
 {
-  odom_pub.publish(msg->pose);
+  geometry_msgs::PoseStamped pose;
+  pose.header = msg->header;
+  pose.pose = msg->pose.pose;
+
+  odom_pub.publish(pose);
 }
 
 int main(int argc, char **argv)
@@ -41,10 +45,26 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "posecov2pose_node");
 
   ros::NodeHandle n;
+  ros::NodeHandle np("~");
+
+  std::string input_odom, output_odom;
+
+  if(!np.getParam("input",input_odom)) {
+    std::cerr << "No input parameter" << std::endl;
+    return 0;
+  }
+
+  if(!np.getParam("output",output_odom)) {
+    std::cerr << "No output parameter" << std::endl;
+    return 0;
+  }
+
+
+
 
   // topics
-  ros::Subscriber odom_sub = n.subscribe<geometry_msgs::PoseWithCovariance>("odom_combined", 1000,poseCallback);
-  ros::Publisher odom_pub = n.advertise<geometry_msgs::Pose>("odom_simple", 1000);
+  ros::Subscriber odom_sub = n.subscribe<geometry_msgs::PoseWithCovarianceStamped>(input_odom, 1000, poseCallback);
+  odom_pub = n.advertise<geometry_msgs::PoseStamped>(output_odom, 1000);
   ros::spin();
 
   return 0;
